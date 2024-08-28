94827117ffad6e10cf42418e1b1ef1acd359f8f0
gauravdubey013 <dubeygaurav520@gmail.com>
2024-08-28 21:47:26 +0530

    added Carousel component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,16 @@
-import Footer from '@/components/core/footer/page';
-import Blog from '@/components/ui/blog/page';
-import Featured from '@/components/ui/featured/page';
-import HomeSection from '@/components/ui/home/page';
-import React from 'react';
-
+import Footer from "@/components/core/footer/page";
+import Blog from "@/components/ui/blog/page";
+import Featured from "@/components/ui/featured/page";
+import HomeSection from "@/components/ui/home/page";
+import React from "react";
 
 export default async function Home() {
   return (
     <>
-  <HomeSection />
-  <Blog /> 
-  <Featured />
-  <Footer />
+      <HomeSection />
+      <Blog />
+      <Featured />
+      <Footer />
     </>
   );
 }
