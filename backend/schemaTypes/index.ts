dc5d7a40458298dede8ff6e249425af56cb12f67
Ɗყηᴀʍɨᴄ ◠‿◕ <adarshpanditdev@gmail.com>
2024-08-29 13:37:01 +0530

    Update in schema

--- backend/schemaTypes/index.ts
+++ backend/schemaTypes/index.ts
@@ -2,5 +2,8 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import mainpost from './mainpost'
+import today from './today'
+import featured from './featured'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, mainpost, today, featured]
